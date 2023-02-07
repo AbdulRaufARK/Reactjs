f9b7cdb22eb459dd8bc26ff140c9658799aeaeb4
Abdul Rauf <40770051+AbdulRaufARK@users.noreply.github.com>
2023-02-07 18:10:25 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,129 +1,73 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
     
-  let mobiles = [
-    {
-      name: "a30",
-      ram: "4gb",
-      rom: "64gb",
-      camera: "50px",
-      price: 2000,
-      brand: "samsung",
-    },
-    {
-      name: "note10",
-      ram: "6gb",
-      rom: "128gb",
-      camera: "50px",
-      price: 4000,
-      brand: "samsung",
-    },
-    {
-      name: "s10",
-      ram: "3gb",
-      rom: "128gb",
-      camera: "10px",
-      price: 5000,
-      brand: "samsung",
-    },
-    {
-      name: "iphone4",
-      ram: "4gb",
-      rom: "64gb",
-      camera: "50px",
-      price: 10000,
-      brand: "iphone",
-    },
-    {
-      name: "iphone4s",
-      ram: "4gb",
-      rom: "64gb",
-      camera: "50px",
-      price: 20000,
-      brand: "iphone",
-    },
-    {
-      name: "iphone5",
-      ram: "4gb",
-      rom: "64gb",
-      camera: "50px",
-      price: 30000,
-      brand: "iphone",
-    },
-    {
-      name: "iphone6",
-      ram: "4gb",
-      rom: "64gb",
-      camera: "50px",
-      price: 30000,
-      brand: "iphone",
-    },
-    {
-      name: "redminote10",
-      ram: "4gb",
-      rom: "64gb",
-      camera: "50px",
-      price: 10000,
-      brand: "xiaomi",
-    },
-    {
-      name: "redminote11",
-      ram: "4gb",
-      rom: "64gb",
-      camera: "50px",
-      price: 10000,
-      brand: "xiaomi",
-    },
-    {
-      name: "redmi10",
-      ram: "4gb",
-      rom: "64gb",
-      camera: "50px",
-      price: 10000,
-      brand: "xiaomi",
-    },
-    {
-      name: "redmi10pro",
-      ram: "4gb",
-      rom: "64gb",
-      camera: "50px",
-      price: 10000,
-      brand: "xiaomi",
-    },
-    {
-      name: "a3s",
-      ram: "4gb",
-      rom: "64gb",
-      camera: "50px",
-      price: 10000,
-      brand: "oppo",
-    },
-    {
-      name: "a3s",
-      ram: "4gb",
-      rom: "64gb",
-      camera: "50px",
-      price: 10000,
-      brand: "Realmi",
-    },
-  ];
-   return(
-    <div>
-      <header className='App-header'>
-      {mobiles.map((x, i) => {
-        return(
-          <div key={i} className="card">
-            <h1 className="heading">{x.brand}</h1>
-            <h2>{x.name}</h2>
-            <h2>{x.camera}</h2>
-            <h2>{x.price}</h2>
+  
+   
 
-          </div>
-        )
-      })}
-      </header>
+
+  const [count,setNames] = useState( []);
+  const [ text,setText] = useState("");
+
+  let add = () => {
+    // setNames(current => [...current, 'abc']);     
+    // console.log(count);
+    count.push(text);
+    // console.log(count)
+    setNames([...count])
+    
+    }
+
+    let edit = (index) =>{
+
+      let value = count[index]
+
+      let editedValue = prompt(value)
+
+    count[index]= editedValue;
+    setNames([...count])
+
+
+     } 
+
+     let deleteVal = (i) => {
+      text.splice(i,0)
+      setText([...text])
+    }
+
+    //  let newArray = count.filter((val,i)=> i !== index);
+
+
+     return(
+    <div>
+      <div>
+          
+          <span>
+          <input onChange={(e) =>{
+            setText(e.target.value)
+          }} type="text" placeholder='Enter value' />
+          <span> <button onClick={add}>ADD</button> </span>
+          </span>
+      </div>
+    {count.map((x,i) => {
+      return(
+        <div>
+          
+          <span><h3>{x}</h3>
+          <span><button onClick={()=>edit(i)}>Edit</button>  </span>
+          <span><button onClick={()=> deleteVal(i)}>Delete</button>  </span>
+          </span> 
+          
+          
+        </div>
+        )})}
+        
     </div>
-   )
+  )
 }
+
+
+export default App;
+
